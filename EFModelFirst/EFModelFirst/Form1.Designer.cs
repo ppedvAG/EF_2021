5a0dff3ae7c9ef7e1ea531eb4105fbdb13201399
André <ruland@outlook.de>
2021-05-26 16:44:22 +0200

    Weitere Queries und Lazy/Eager Loading

--- EFModelFirst/EFModelFirst/Form1.Designer.cs
+++ EFModelFirst/EFModelFirst/Form1.Designer.cs
@@ -33,6 +33,8 @@ namespace EFModelFirst
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -42,7 +44,9 @@ namespace EFModelFirst
             this.flowLayoutPanel1.AutoSize = true;
             this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
             this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -71,18 +75,43 @@ namespace EFModelFirst
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(855, 422);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
             // 
             // button2
             // 
             this.button2.AutoSize = true;
             this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button2.Location = new System.Drawing.Point(140, 3);
+            this.button2.Location = new System.Drawing.Point(252, 3);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(258, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Demodaten erzeugen";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.Location = new System.Drawing.Point(140, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Fred #6";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button4.Location = new System.Drawing.Point(516, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 42);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Count";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
@@ -110,6 +139,8 @@ namespace EFModelFirst
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- EFModelFirst/EFModelFirst/Form1.cs
+++ EFModelFirst/EFModelFirst/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
 
 namespace EFModelFirst
 {
@@ -21,7 +22,13 @@ namespace EFModelFirst
 
         private void button1_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = context.PersonSet.ToList();
+            dataGridView1.DataSource = context.PersonSet.OfType<Mitarbeiter>()
+                                                        .Include(x => x.Abteilung) //eager Loading
+                                                        .Include(x => x.Kunde) //eager Loading
+                                                        .Where(x => x.Gebdatum.Month > 6 && x.Name.StartsWith("F"))
+                                                        .OrderBy(x => x.Gebdatum.Day)
+                                                        .ThenByDescending(x => x.Gebdatum.Year)
+                                                        .ToList();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -33,18 +40,43 @@ namespace EFModelFirst
             {
                 Mitarbeiter m = new Mitarbeiter();
                 m.Name = $"Fred #{i:000}";
-                m.Beruf= $"Macht dinge";
+                m.Beruf = $"Macht dinge";
                 m.Gebdatum = DateTime.Now.AddYears(-40).AddDays(i * 17);
 
                 if (i % 2 == 0)
                     m.Abteilung.Add(abt1);
-                
+
                 if (i % 3 == 0)
                     m.Abteilung.Add(abt2);
 
                 context.PersonSet.Add(m);
             }
             context.SaveChanges();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var fred6 = context.PersonSet.OfType<Mitarbeiter>().FirstOrDefault(x => x.Id == 5);
+
+            if (fred6 != null)
+            {
+                MessageBox.Show($"{fred6.Name} {fred6.Gebdatum:d} {string.Join(",", fred6.Abteilung.Select(x => x.Bezeichnung))}");
+
+            }
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.Value is IEnumerable<Abteilung> abts)
+            {
+                e.Value = string.Join(" 🚕 ", abts.Select(x => x.Bezeichnung));
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int count = context.PersonSet.Count(x => x.Gebdatum.Month == 6);
+            MessageBox.Show($"{count} Personen");
         }
     }
 }
