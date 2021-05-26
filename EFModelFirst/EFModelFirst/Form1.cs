423933dc6f7998a92c692e249462857dd5c17c67
André <ruland@outlook.de>
2021-05-26 15:41:46 +0200

    EfModelFirst

---
+++ EFModelFirst/EFModelFirst/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFModelFirst
+{
+    public partial class Form1 : Form
+    {
+        Model1Container context = new Model1Container();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = context.PersonSet.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Abteilung abt1 = new Abteilung() { Bezeichnung = "Steine" };
+            Abteilung abt2 = new Abteilung() { Bezeichnung = "Holz" };
+
+            for (int i = 0; i < 100; i++)
+            {
+                Mitarbeiter m = new Mitarbeiter();
+                m.Name = $"Fred #{i:000}";
+                m.Beruf= $"Macht dinge";
+                m.Gebdatum = DateTime.Now.AddYears(-40).AddDays(i * 17);
+
+                if (i % 2 == 0)
+                    m.Abteilung.Add(abt1);
+                
+                if (i % 3 == 0)
+                    m.Abteilung.Add(abt2);
+
+                context.PersonSet.Add(m);
+            }
+            context.SaveChanges();
+        }
+    }
+}
